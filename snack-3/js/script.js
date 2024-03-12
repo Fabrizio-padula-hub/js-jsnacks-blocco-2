a1c0b5abf91dee765a3e22daa935cb46141890d3
Fabrizio-padula-hub <fabriziopadula80@gmail.com>
2024-03-12 15:52:22 +0100

    feat: add array and if

--- snack-3/js/script.js
+++ snack-3/js/script.js
@@ -2,3 +2,22 @@
 // un div avrà il testo colorato di rosso mentre l’altro di verde.
 // Partendo da un array di numeri, stampiamo nell’id rosso i numeri dispari e 
 // in verde i numeri pari.
+
+// avendo un array con dei numeri
+const listNumber = [20, 40, 33, 28, 3, 56, 98, 45, 87, 39, 51, 44, 12, 17, 6, 9, 100, 103, 35, 63];
+    console.log(listNumber)
+    
+    // comparare se i numeri sono pari o dispari
+    if(listNumber % 2 === 0){
+
+    }
+
+
+
+
+
+
+
+
+// prendere i numeri pari e spostarli nel div verde
+// prendere i numeri dispari e spostarli nel div rosso
