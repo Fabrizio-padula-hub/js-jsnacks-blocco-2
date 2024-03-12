3c50e898e6a67b1b03ceed1fc9ea1d3cf348bb24
Fabrizio-padula-hub <fabriziopadula80@gmail.com>
2024-03-12 15:15:14 +0100

    feat: add number even or odd

--- snack-1/js/script.js
+++ snack-1/js/script.js
@@ -1,3 +1,19 @@
 // L'utente inserisce un numero nel prompt, se è pari stampa il numero,
 // se è dispari stampa il numero successivo
 
+// chiedere all'utente il numero
+const numberUser = parseInt(prompt('Scrivi un numero'));
+
+// verificare il numero dell'utente se è pari o dispari
+// se è pari stampare il numero
+// se è dispari 
+// stampare il numero successivo
+if (numberUser % 2 ===  0) {
+    alert(`Il tuo numero ${numberUser} è pari`);
+} else if (numberUser % 2 !==  0) {
+    alert(`Il numero che hai scritto non è pari, stampo il successivo ${numberUser + 1}`);
+}
+
+
+    
+    
