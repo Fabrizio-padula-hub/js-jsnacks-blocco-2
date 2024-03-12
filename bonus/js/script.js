bb1842ba875430654cef2b41af9416ff65f74c24
Fabrizio-padula-hub <fabriziopadula80@gmail.com>
2024-03-12 16:27:25 +0100

    feat: add bonus and structure

---
+++ bonus/js/script.js
@@ -0,0 +1,11 @@
+// Chiedi un numero di 4 cifre all’utente e 
+// calcola la somma di tutte le cifre che compongono il numero.
+
+
+
+// chiedere all'utente un numero di 4 cifre
+const numberUser = parseInt(prompt('Scrivi un numero di 4 cifre'));
+console.log(numberUser)
+
+// vedere i numeri
+// calcolare la somma di tutte le cifre che compongono il numero.
